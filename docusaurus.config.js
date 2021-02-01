1e7cde8e6c0845636c4605b6b2eb1d7e6bc3113b
Sayed Hadi Hashemi <SayedHadiHashemi@gmail.com>
2021-01-31 23:30:32 -0800

    Fix edit page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,13 +115,13 @@ module.exports = {
                     sidebarPath: require.resolve('./sidebars.js'),
                     // Please change this to your repo.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/edit/master/website/',
+                        'https://github.com/genoml/genoml.github.io/edit/main/',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                        'https://github.com/genoml/genoml.github.io/edit/main/blog/',
                 },
                 theme: {
                     customCss: [
